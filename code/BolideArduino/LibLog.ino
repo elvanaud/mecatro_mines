206adcab58a33e2ddee792f9dabe0fca29d923a9
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2023-10-04 17:03:45 +0200

    Added sensor and telemetry code

---
+++ code/BolideArduino/LibLog.ino
@@ -0,0 +1,37 @@
+#if TELEMETRY
+template<typename T>
+void println(T str){
+  ;
+}
+
+template<typename T>
+void print(T str){
+  ;
+}
+
+void print(){
+  ;
+}
+
+void println(){
+  ;
+}
+#else
+template<typename T>
+void println(T str){
+  Serial.println(str);
+}
+
+template<typename T>
+void print(T str){
+  Serial.println(str);
+}
+
+void print(){
+  Serial.println();
+}
+
+void println(){
+  Serial.println();
+}
+#endif
